b908c2dfdfdfe738f42b9c8de98ad97949155acb
Eskyny <125225752+Eskyny@users.noreply.github.com>
2026-04-27 19:52:15 +0200

    Enhance on_ready event to display server count
    Added server count printout on bot ready event.

--- selfbot.py
+++ selfbot.py
@@ -1,10 +1,11 @@
 import discord
 
-client = discord.Client()
+client = discord.Client(chunk_guilds_at_startup=False)
 
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.dnd) # Do not disturb
     print(f'Connected as {client.user} !')
+    print(f'Servers: {len(client.guilds)}')
 
 client.run('TOKEN_HERE')
